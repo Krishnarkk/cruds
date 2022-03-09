18d5c33c6ff2f2e9a21aa115694e6a15da139144
Krishnajireact <86544748+Krishnarkk@users.noreply.github.com>
2022-03-09 21:24:42 +0530

    Initial commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Typography,Box,makeStyles,Grid,TableContainer,Table,TableBody,TableCell,TableHead,TableRow,Paper,Tooltip,TextField,Button} from '@mui/material'
+const Home = () => {
+  return (
+    <div>
+         <Typography variant="body2" color="text.secondary">Homess</Typography>Home</div>
+  )
+}
+
+export default Home
